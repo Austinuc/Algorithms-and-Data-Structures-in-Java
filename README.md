3000a1b0228ff43b7551dc24d9e124754a5ddafb
Austin <austin5astro@gmail.com>
2022-09-21 20:54:39 +0100

    First commit: added a method that computes the running sum of a 1d Array

---
+++ README.md
@@ -0,0 +1,6 @@
+# Algorithms and Data Structures in Java
+This showcases the solutions to algorithm and data structure challenges in Java
+
+Algorithm Challenge | Description
+--- | ---
+Running Sum of 1d Array | Given an array of nums. We define a running sum of an array as runningSum[i] = sum(nums[0]...nums[i]). eg: Input: nums = [1,2,3,4] Output: [1,3,6,10]
---
+++ RunningSumOf1dArray.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+/**
+ * Running Sum Of 1d Array: Given an array nums. We define a running sum of
+ *                         an array as runningSum[i] = sum(nums[0]…nums[i])
+ */
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += sum;
+            sum = nums[i];
+
+        }
+        return nums;
+    }
+}
