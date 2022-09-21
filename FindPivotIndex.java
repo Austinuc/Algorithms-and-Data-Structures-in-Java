d4260858976c0f5487176e7496e8839a778f2037
Austin <austin5astro@gmail.com>
2022-09-21 21:33:37 +0100

    Added a method that finds the pivot index of a given integer array

---
+++ FindPivotIndex.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+/**
+ * pivotIndex - Finds the pivot index of a given array
+ * @nums: input arrays of integers
+ * Description: The pivot index is the index where the sum of all the numbers strictly
+ *             to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+ * Return: Return the leftmost pivot index. If no such index exists, return -1.
+ */
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int totalArraySum = 0, leftSum = 0;
+
+        if (nums.length == 1)
+            return 0;
+        for(int i = 0; i < nums.length; i++) {
+            totalArraySum += nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+
+            if (totalArraySum - nums[i] == leftSum) //check if leftsum equals rightsum removing the current element
+                return i;
+            totalArraySum -=nums[i];
+            leftSum += nums[i];
+        }
+        return -1;
+
+    }
+}
