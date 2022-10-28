1acec3a1041ce74a0cef71f8cf29f35af4d71403
Austin <augustine.igboke@decagon.dev>
2022-10-28 02:21:31 +0100

    Added files

--- src/main/java/Arrays_Algorithms/RangeCompactArray.java
+++ src/main/java/Arrays_Algorithms/RangeCompactArray.java
@@ -25,7 +25,11 @@ public class RangeCompactArray {
                         ans += (k==0) ? arr.get(k++) : ","+arr.get(k++);
                 } else if (count>=3 && k==0) //don't add comma while printing the first element
                     ans += arr.get(k)+"-"+arr.get(j-1);
-                else //print from k - j if you're at the last element or from k - j-1 otherwise
+                else if (j == arr.size()-1 && arr.get(j)-arr.get(j-1) != 1) {
+                    ans += ","+arr.get(k)+"-"+arr.get(j-1);
+                    ans += ","+arr.get(j);
+                    break;
+                } else //print from k - j if you're at the last element or from k - j-1 otherwise
                     ans += (j == arr.size()-1) ? ","+arr.get(k)+"-"+arr.get(j) : ","+arr.get(k)+"-"+arr.get(j-1);
 
                 k = j; //move you starting point to your current position
@@ -39,7 +43,8 @@ public class RangeCompactArray {
     }
 
     public static void main(String[] args) {
-        rangeCompact(new ArrayList<Integer>(Arrays.asList(-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20)));
-        rangeCompact(new ArrayList<Integer>(Arrays.asList(-3, -2, -1, 2, 10, 15, 16, 18, 19, 20)));
+        //rangeCompact(new ArrayList<Integer>(Arrays.asList(-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20)));
+        //rangeCompact(new ArrayList<Integer>(Arrays.asList(-3, -2, -1, 2, 10, 15, 16, 18, 19, 20)));
+        rangeCompact(new ArrayList<Integer>(Arrays.asList(-48, -45, -42, -41, -38, -36, -34, -31, -29, -28, -27, -25, -24, -21, -19, -18, -16, -14, -13, -12, -11, -10, -7)));
     }
 }
